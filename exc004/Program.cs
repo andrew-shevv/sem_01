793095840b889b0580ff4e7546f3e8dd8e636345
Andrew <vishnyaishere@gmail.com>
2022-08-16 23:01:26 +0300

    slight correction

--- exc004/Program.cs
+++ exc004/Program.cs
@@ -2,8 +2,9 @@
 // которая принимает на вход трёхзначное число 
 // и на выходе показывает последнюю цифру этого числа.
 
-Console.WriteLine("Input number: ");
+Console.WriteLine("Input three-digit number: ");
 
 int num = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(num % 10);
+if (num > 99 && num < 1000) Console.WriteLine(num % 10);
+else Console.WriteLine("This number doesn't have three digits");
