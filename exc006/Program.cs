e88e1fa8b077a33156eed9d60eb3c53dfee3b963
Andrew <vishnyaishere@gmail.com>
2022-08-16 22:42:59 +0300

    added exc006

---
+++ exc006/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, 
+// которая принимает на вход три числа 
+// и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Input three numbers: ");
+
+int numA = Convert.ToInt32(Console.ReadLine());
+int numB = Convert.ToInt32(Console.ReadLine());
+int numC = Convert.ToInt32(Console.ReadLine());
+
+if (numA > numB && numA > numC) Console.WriteLine("max = " + numA);
+else 
+    if (numB > numA && numB > numC) Console.WriteLine("max = " + numB);
+else 
+    if (numC > numA && numC > numB) Console.WriteLine("max = " + numC);
+else Console.WriteLine("The numbers are equal");
