45188153d899af41cd5dd9a44c97928172b358c5
Andrew <vishnyaishere@gmail.com>
2022-08-16 21:28:11 +0300

    added exc001

---
+++ exc001/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, 
+// которая на вход принимает два числа и проверяет, 
+// является ли первое число квадратом второго.
+
+Console.WriteLine("Input two numbers: ");
+
+int numA = Convert.ToInt32(Console.ReadLine());
+int numB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Is the first number the square of the second number?");
+
+if (numA == numB * numB)
+{
+    Console.WriteLine("yes");
+}
+else Console.WriteLine("no");
