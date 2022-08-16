fb9dd10b086a8353375360608c38bf4e543ff181
Andrew <vishnyaishere@gmail.com>
2022-08-16 22:17:59 +0300

    added exc003

--- exc003/Program.cs
+++ exc003/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, 
+// которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("Input number: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+int count = -N;
+
+while (count <= N)
+{
+    Console.Write(count + " ");
+    count += 1;
+}
