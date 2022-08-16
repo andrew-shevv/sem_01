b49eb56147d6f86db8059f103291618d792c84f9
Andrew <vishnyaishere@gmail.com>
2022-08-16 22:24:08 +0300

    added exc004

---
+++ exc004/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу,
+// которая принимает на вход трёхзначное число 
+// и на выходе показывает последнюю цифру этого числа.
+
+Console.WriteLine("Input number: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(num % 10);
