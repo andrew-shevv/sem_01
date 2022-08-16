91a2b0a428777556d20e2ff5d8de2b339412b0c3
Andrew <vishnyaishere@gmail.com>
2022-08-16 22:33:07 +0300

    added exc005 + minor formatting of exc002

---
+++ exc005/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу,
+// которая на вход принимает два числа
+// и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Input two numbers: ");
+
+int numA = Convert.ToInt32(Console.ReadLine());
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numA > numB) Console.WriteLine("max = " + numA);
+else 
+    if (numB > numA) Console.WriteLine("max = " + numB);
+else Console.WriteLine("The numbers are equal");
